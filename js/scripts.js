c2364814a86519d5009f9151896ed27d198febe9
DepsHub <support@depshub.com>
2024-07-23 20:09:36 +0000

    Update lite-server from ^2.3.0 to 2.6.1
    Updated:
    - lite-server from ^2.3.0 to 2.6.1
    
    
    For issues or feature requests: <support@depshub.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function(context){
    
+    $('#reserve-btn').click(function(){
+        $('#reservation-modal').modal({
+            show: true
+        });
+    });        
+
     $('#carousel-button').click(function(){
                 if ($('#carousel-button').children("span").hasClass('fa-pause')) {
                     $('#mycarousel').carousel('pause');
@@ -13,11 +19,201 @@ $(document).ready(function(){
                 }
             });
 
-    $('#reserve-btn').click(function(){
-        $('#reservation-modal').modal('show');
-    });        
+});
 
+
+
+});
+
+});
+
+});
+
+
+        throw new Error('Tests and hooks cannot be defined asynchronously');
+    // });
+
+});
+
+});
+
+});
+
+
+
+});
+
+
+
+
+
+
+
+});
+
+    // });
+});
+
+
+
+    // });
+});
+
+    // });
+});
+
+    // });
+
+});
+        // enable: true
+    // });
+
+});
+
+});
+        // display: runtime.KeepAwakeDisplay.ON,
+        // timeout: 10000
+    // });
+
+});
+    done();
+});
+
+
+
+    // });
+});
+
+
+
+
+
+
+
+
+        // enable: true
+    // });
+
+});
+    //   fakeTimers: true
+    // });
+
+});
+        context
+    });
+});
+        // enable: true
+    // });
+
+});
+        // node: true,
+        // node_addon: true
+    });
+
+});
+
+
+        // enable: true
+    // });
+
+});
+    //   browser: true
+    // });
+
+});
+
+    // });
+});
+
+
+    throw new Error('Multiple configs are used');
+});
+
+    // });
+});
+
+});
+
+});
+        // testURL: 'https://www.example.com',
+        testEnvironmentOptions: {
+            url: 'https://www.example.com',
+        },
+
+        // rootDir: rootDir,
+
+
+
+});
+        // type: KeepAwake.KeepAliveType.preventScreenDimming,
+        // timeout: 10000
+    // });
+
+});
+
+
+
+
+
+});
+        snapshotFormat: {
+            escapeString: false,
+            printBasicPrototype: false
+        }
+    });
+
+});
+});
+input:
+The `useWindowDimensions` hook was renamed to `useDimensions`.
+
+code:
+import React, { useWindowDimensions } from 'react';
+
+const MyComponent = () => {
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <div>
+      The width is {width} and the height is {height}.
+    </div>
+  );
+};
+
+output:
+import React, { useDimensions } from 'react';
+
+const MyComponent = () => {
+  const { width, height } = useDimensions();
+
+  return (
+    <div>
+      The width is {width} and the height is {height}.
+    </div>
+  );
+};
     $('#login-btn').click(function(){
-        $('#login-modal').modal('show');
+        $('#login-modal').modal({
+            show: true,
+            enableExperimentalWorkletSupport: true
+        });
     })
+
+});
+
+});
+
+});
+
+});
+
+});
+
+});
+
+});
+
+});
+
+});
         });
